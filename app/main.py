1a573e648b4bafe684c02d2a7630e0f69c383abc
Kasra Faraji <kasrafaraj2007@gmail.com>
2026-06-02 23:35:26 +0330

    chore: dockerize project with postgresql and redis

--- app/main.py
+++ app/main.py
@@ -7,12 +7,21 @@ from fastapi.security import OAuth2PasswordBearer
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 ##models.Base.metadata.create_all(bind = engine)
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
 
 app = FastAPI(title="E-Commerce API")
 
 app.include_router(auth.router)
 app.include_router(products.router)
 app.include_router(cart.router)
+
+@app.on_event("startup")
+async def on_startup():
+    await init_db()
 
 @app.get("/hello")
 def say_hi():
